ad0167967861cdc910c0035f594e4ac25fd85df4
Hemendra <hsahu003@gmail.com>
2024-06-01 12:31:58 +0530

    database related changes

--- src/main/java/com/hemendrasahu/productservice/models/Product.java
+++ src/main/java/com/hemendrasahu/productservice/models/Product.java
@@ -1,14 +1,19 @@
 package com.hemendrasahu.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
-    private String category;
+
+    @ManyToOne
+    private Category category;
     private double price;
 }
