aaf383b4cd1697d3d833d3f1608bdf276a0e0879
Hemendra <hsahu003@gmail.com>
2024-06-03 19:54:05 +0530

    minor changes

--- src/main/java/com/hemendrasahu/productservice/repository/ProductRepository.java
+++ src/main/java/com/hemendrasahu/productservice/repository/ProductRepository.java
@@ -3,8 +3,6 @@ package com.hemendrasahu.productservice.repository;
 import com.hemendrasahu.productservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
